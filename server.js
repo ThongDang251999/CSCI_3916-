2019f1dea4b310c30b34e3ca7d6231b215e96b2e
Thong Dang <minhthong251999@gamil.com>
2025-02-09 21:44:00 -0700

    commit Hw1-3916

---
+++ server.js
@@ -0,0 +1,25 @@
+//pulling in expres to create the API server 
+const express = require('express');
+const app = express(); 
+
+app.use(express.json()); //middleware that parses JSON in the request body 
+
+//creating a route that listens for GET requests on the root URL 
+app.post('/', (req, res) => {
+    const acceptHeader = req.get['accept'];
+
+    const responseBody = { 
+        acceptHeader: acceptHeader, 
+        ...req.body
+    }
+
+    res.json(resposneBody)
+});
+
+const port = process.env.PORT || 3000; 
+const server = app.listen(port, () => { 
+    console.log(`Server is running on port ${port}`); 
+
+});
+
+module.exports = server;  
---
+++ test/echo.test.js
@@ -0,0 +1,27 @@
+const chai = require('chai'); 
+const chaiHttp = require('chai-http'); 
+const { expect } = chai; 
+const server = require('../server'); 
+
+chai.use(chaiHttp); 
+
+describe('POST /', () => {
+    it('should reutrn a JSON object with the accept header', (done) =>{
+        const sampleBody = { message: "Hello, World!"}; 
+
+        chai
+            .request(server)
+            .post('/')
+            .set('accpet', 'application/json')
+            .send(sampleBody)
+            .end((err, response) => { 
+                expect(err).to.be.null; 
+                expect(response).to.have.status(200); 
+                expect(response).to.be.json; 
+                expect(response.body).to.have.property('message', "Hello, World!");
+                done();
+            }); 
+
+    });
+
+});
